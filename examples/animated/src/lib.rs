37458c83b233a26dfed3ebe73a14b13d280c79c4
Anatol Ulrich <anatol@versteht.es>
2022-04-10 23:40:57 +0200

    snapshot

--- examples/animated/src/lib.rs
+++ examples/animated/src/lib.rs
@@ -1,19 +1,18 @@
-use embedded_graphics::{draw_target::DrawTarget, prelude::*, primitives::Rectangle};
-use std::cell::RefCell;
-use std::rc::Rc;
-use wasm_bindgen::prelude::*;
-use wasm_bindgen::JsCast;
+use std::{cell::RefCell, rc::Rc};
 
+use eg_bdf::{include_bdf, text::BdfTextStyle, BdfFont};
+use embedded_graphics::{
+    draw_target::DrawTarget,
+    pixelcolor::{Rgb565, Rgb888},
+    prelude::{Point, Primitive, WebColors, *},
+    primitives::{Circle, PrimitiveStyle, Rectangle},
+    text::Text,
+    Drawable,
+};
 use embedded_graphics_web_simulator::{
     display::WebSimulatorDisplay, output_settings::OutputSettingsBuilder,
 };
-
-use embedded_graphics::{
-    pixelcolor::Rgb565,
-    prelude::{Point, Primitive, WebColors},
-    primitives::{Circle, PrimitiveStyle},
-    Drawable,
-};
+use wasm_bindgen::{prelude::*, JsCast};
 
 fn window() -> web_sys::Window {
     web_sys::window().expect("no global `window` exists")
@@ -37,7 +36,9 @@ fn text_container() -> web_sys::Element {
         .expect("document should have our text container")
 }
 
-const NUM_ITER: i32 = 60;
+const NUM_ITER: i32 = 6000;
+
+const AW_MINTS: BdfFont = include_bdf!("/Users/ace/play/clone/artwiz-fonts-wl/mints-mild-bold.bdf");
 
 // This function is automatically invoked after the wasm module is instantiated.
 #[wasm_bindgen(start)]
@@ -47,14 +48,19 @@ pub fn run() -> Result<(), JsValue> {
     #[cfg(debug_assertions)]
     console_error_panic_hook::set_once();
 
+    let aw_style = BdfTextStyle::new(&AW_MINTS, Rgb888::MAGENTA);
+
     let document = document();
     let output_settings = OutputSettingsBuilder::new()
-        .scale(3)
-        .pixel_spacing(1)
+        .scale(8)
+        .pixel_spacing(3)
         .build();
     let size = (2 * NUM_ITER) as u32;
+
+    const W: usize = 12;
+    const H: usize = 8;
     let mut img_display = WebSimulatorDisplay::new(
-        (size, size),
+        (W as u32, H as u32),
         &output_settings,
         document.get_element_by_id("graphics").as_ref(),
     );
@@ -77,6 +83,8 @@ pub fn run() -> Result<(), JsValue> {
 
     let mut i = 0;
 
+    let mut backing = [rgb::RGB8::default(); { W * H }];
+
     *g.borrow_mut() = Some(Closure::wrap(Box::new(move || {
         if i > NUM_ITER {
             text_container().set_text_content(Some("All done!"));
@@ -89,22 +97,42 @@ pub fn run() -> Result<(), JsValue> {
 
         // Set the body's text content to how many times this
         // requestAnimationFrame callback has fired.
-        i += 1;
         let text = format!("requestAnimationFrame has been called {} times.", i);
         text_container().set_text_content(Some(&text));
 
-        img_display.clear(Rgb565::BLACK).expect("could not clear()");
+        img_display.clear(Rgb888::BLACK).expect("could not clear()");
 
-        for j in 0..5 {
-            Circle::new(
-                Point::new(NUM_ITER - i - 2 * j, NUM_ITER - i - 2 * j),
-                (2 * i + 4 * j) as u32,
+        // let area = Rectangle::new(Point::new(0, 0), Size::new(W, H));
+
+        // let pixels = area.points().map(|point| {
+        //     let xy_extrawurst = Vector2::new(point.x as f32, point.y as f32);
+        //     Pixel(point, qc.pixel(xy_extrawurst, time))
+        // });
+
+        // img_display.draw_iter(pixels).expect("could not draw..?");
+        // effects::rainborrok(i as u16, 0.9, 0.15, 1.0, &mut backing);
+
+        effects::chaser(i as u16, &mut backing);
+
+        let it = backing.iter().enumerate().map(|(idx, val)| {
+            Pixel(
+                Point::new((idx % W) as i32, (idx / W) as i32),
+                Rgb888::new(val.r, val.g, val.b),
             )
-            .into_styled(PrimitiveStyle::with_stroke(Rgb565::CSS_PINK, 1))
+        });
+
+        img_display.draw_iter(it).expect("no drawing the thing?");
+
+        let scroll = i / 2;
+        let x = 10 - (scroll % 45);
+        let y = 6;
+        let position = Text::new("CHKR", Point::new(x, y), aw_style)
             .draw(&mut img_display)
-            .expect("could not draw Circle");
-        }
+            .unwrap();
+
         img_display.flush().expect("could not flush buffer");
+
+        //utils::led::segmented(data, zigzag)
 
         // Schedule ourself for another requestAnimationFrame callback.
         request_animation_frame(f.borrow().as_ref().unwrap());
